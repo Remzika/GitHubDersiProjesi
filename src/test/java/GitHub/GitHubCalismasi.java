c5973302f7d401163e648538bc28a72f23a934e5
remzi <remzika58@gmail.com>
2022-04-06 19:31:38 +0200

    ilk gonderim

---
+++ src/test/java/GitHub/GitHubCalismasi.java
@@ -0,0 +1,11 @@
+package GitHub;
+
+import java.util.Arrays;
+
+public class GitHubCalismasi {
+
+
+    public static void main(String[] args) {
+        System.out.println("Github dersi Basladi");
+    }
+}
